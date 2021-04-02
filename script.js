c67fae10ea9c1ae0f1cfa4e2333ac1180f6bd538
Andrés <andresuriegas@gmail.com>
2021-04-02 13:43:02 +0200

    Add script and styles for closing nav when user clicks outside nav

--- script.js
+++ script.js
@@ -13,19 +13,48 @@ const navSlide = () => {
       if (link.style.animation) {
         link.style.animation = "";
       } else {
-        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 0.5
+        }s`;
       }
     });
     //Burger Animation
-    burger.classList.toggle('toggle')
+    burger.classList.toggle("toggle");
   });
 };
+
+// Close hamburger menu if user clicks outside
+document.addEventListener("click", (event) => {
+  const navigation = document.getElementById("navigation");
+  const nav = document.querySelector(".nav-links");
+  const burger = document.querySelector(".burger");
+  const navLinks = document.querySelectorAll(".nav-links li");
+
+  if (
+    !navigation.contains(event.target) &&
+    nav.classList.contains("nav-active")
+  ) {
+    nav.classList.toggle("nav-active");
+
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 0.5
+        }s`;
+      }
+    });
+
+    burger.classList.toggle("toggle");
+  }
+});
 
 navSlide();
 
 //Update value of --vh when the window resizes
-window.addEventListener('resize', () => {
-    // Get inner height of the viewport
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-})
+window.addEventListener("resize", () => {
+  // Get inner height of the viewport
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+});
