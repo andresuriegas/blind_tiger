00f037da71a8c65d9d301f0edd96d70b45ce890e
Andrés <andresuriegas@gmail.com>
2021-04-21 05:48:41 +0200

    Remove unused code from script and tune styles

--- script.js
+++ script.js
@@ -84,17 +84,4 @@ const topFunction = () => {
   document.documentElement.scrollY = 0;
 };
 
-const background = document.querySelector('.background');
-
-document.addEventListener('scroll', () => {
-    const scrollY = window.scrollY;
-
-    if(scrollY !== 0) {
-        background.style.backgroundPosition = `calc(50% + ${scrollY}px) calc(50% + ${scrollY}px)`;
-    } else {
-        background.style.backgroundPosition = '';
-    }
-});
-
-
 const button = document.getElementById('topBtn').style.visibility = "hidden"
